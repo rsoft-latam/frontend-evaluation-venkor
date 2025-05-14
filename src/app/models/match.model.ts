bf110c0f9b054ba4765e9da34d32491592d249da
Ricardo Pari <rikiricardo16@hotmail.com>
2025-05-14 19:02:16 -0400

    feat: modify league service to add functionality in fetchData, setMatches and getMatches

---
+++ src/app/models/match.model.ts
@@ -0,0 +1,9 @@
+export interface IMatch {
+  awayTeam: string;
+  awayTeamScore: number;
+  homeTeam: string;
+  homeTeamScore: number;
+  matchDate: number;
+  matchPlayed: boolean;
+  stadium: string;
+}
--- src/app/services/league.service.ts
+++ src/app/services/league.service.ts
@@ -1,21 +1,25 @@
 /**
  * A class representing a service that processes the data for match schedule
  * and generates leaderboard.
- * 
- * NOTE: MAKE SURE TO IMPLEMENT ALL EXISITNG METHODS BELOW WITHOUT CHANGING THE INTERFACE OF THEM, 
- *       AND PLEASE DO NOT RENAME, MOVE OR DELETE THIS FILE.  
- * 
+ *
+ * NOTE: MAKE SURE TO IMPLEMENT ALL EXISITNG METHODS BELOW WITHOUT CHANGING THE INTERFACE OF THEM,
+ *       AND PLEASE DO NOT RENAME, MOVE OR DELETE THIS FILE.
+ *
  */
 
-import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IMatch } from '../models/match.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class LeagueService {
+  private _baseUrl: string = environment.apiBaseUrl;
+  private _matches: IMatch[] = [];
 
-  constructor () {}
+  constructor(private _http: HttpClient) {}
 
   /**
    * Sets the match schedule.
@@ -44,14 +48,18 @@ export class LeagueService {
    * @param {Array} matches List of matches.
    */
 
-  setMatches (matches: any[]) {}
+  setMatches(matches: IMatch[]) {
+    this._matches = matches;
+  }
 
   /**
    * Returns the full list of matches.
    *
    * @returns {Array} List of matches.
    */
-  getMatches () {}
+  getMatches() {
+    return this._matches;
+  }
 
   /**
    * Returns the leaderBoard in a form of a list of JSON objecs.
@@ -68,13 +76,22 @@ export class LeagueService {
    *
    * @returns {Array} List of teams representing the leaderBoard.
    */
-   getLeaderBoard () {}
+  getLeaderBoard() {}
 
   /**
    * Asynchronic function to fetch the data from the server and set the matches.
    */
-  async fetchData () {
-    const matches = [] //TODO: replace this with the correct matches.
-    this.setMatches(matches); 
+  async fetchData() {
+    try {
+      const response: { success: boolean; matches: IMatch[] } = await this._http
+        .get<{ success: boolean; matches: IMatch[] }>(
+          `${this._baseUrl}/api/v1/getAllMatches`
+        )
+        .toPromise();
+      const matches = response.matches; //TODO: replace this with the correct matches.
+      this.setMatches(matches);
+    } catch (err) {
+      console.error('Error fetching data:', err);
+    }
   }
 }
