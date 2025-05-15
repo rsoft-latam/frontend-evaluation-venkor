146deb9a3c4800878a9615fa27cf404cb03c9f0b
Ricardo Pari <rikiricardo16@hotmail.com>
2025-05-15 02:35:35 -0400

    feat: unit test for ng-footer component

--- src/app/layout/ng-footer/ng-footer.component.spec.ts
+++ src/app/layout/ng-footer/ng-footer.component.spec.ts
@@ -1,25 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgFooterComponent } from './ng-footer.component';
+import { AppInfoService } from 'src/app/services/app-info.service';
+import { of } from 'rxjs';
 
 describe('NgFooterComponent', () => {
   let component: NgFooterComponent;
   let fixture: ComponentFixture<NgFooterComponent>;
+  let mockAppInfoService: jest.Mocked<AppInfoService>;
 
   beforeEach(async () => {
+    mockAppInfoService = {
+      getApiVersion: jest
+        .fn()
+        .mockReturnValue(of({ success: true, version: '' })),
+    } as unknown as jest.Mocked<AppInfoService>;
+
     await TestBed.configureTestingModule({
-      declarations: [ NgFooterComponent ]
-    })
-    .compileComponents();
+      declarations: [NgFooterComponent],
+      providers: [{ provide: AppInfoService, useValue: mockAppInfoService }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NgFooterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set apiVersion on successful response', () => {
+    const mockResponse = { success: true, version: '1.2.3' };
+    mockAppInfoService.getApiVersion.mockReturnValue(of(mockResponse));
+
+    fixture.detectChanges();
+
+    expect(component.apiVersion).toBe('1.2.3');
+  });
+
+  it('should not set apiVersion on failure', () => {
+    const mockResponse = { success: false, version: 'should-not-set' };
+    mockAppInfoService.getApiVersion.mockReturnValue(of(mockResponse));
+
+    fixture.detectChanges();
+
+    expect(component.apiVersion).toBe('');
   });
 });
