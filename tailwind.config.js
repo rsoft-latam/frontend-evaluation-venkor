17d243ff78c40d375d02856bdd4a02b7d2c76eb4
Ricardo Pari <rikiricardo16@hotmail.com>
2025-05-15 01:34:42 -0400

    feat: tailwind configuration for fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        open: ['"Open Sans"', 'sans-serif'],
+      },
+      fontSize: {
+        menu: '16px',
+        heading: '24px',
+        tableHeader: '12px',
+        tableCell: '14px',
+        tableCellBold: '16px',
+      },
+      colors: {
+        menu: '#FFFFFF',
+        heading: '#182C62',
+        tableText: '#4B5C68',
+        footerText: '#4B5C68',
+        headerBg: '#025FEB',
+        tableHeaderBg: '#E4EDF2',
+        tableBorder: '#E4EDF2',
+        rowEven: '#F6F7F7',
+        footerBg: '#F6F7F7',
+      },
+    },
   },
   plugins: [],
 }
