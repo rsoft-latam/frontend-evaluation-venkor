65a8f66a5827dd44d115bb1d59f5ab8e11bfdde7
Ricardo Pari <rikiricardo16@hotmail.com>
2025-05-15 02:50:01 -0400

    feat: unit test for auth-interceptor

--- src/app/core/interceptors/auth.interceptor.spec.ts
+++ src/app/core/interceptors/auth.interceptor.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthInterceptor } from './auth.interceptor';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { AuthService } from 'src/app/services/auth.service';
 
 describe('AuthInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthInterceptor
-      ]
-  }));
+  let http: HttpClient;
+  let httpMock: HttpTestingController;
+  let mockAuthService: { getToken: jest.Mock };
 
-  it('should be created', () => {
-    const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
-    expect(interceptor).toBeTruthy();
+  beforeEach(() => {
+    mockAuthService = {
+      getToken: jest.fn(),
+    };
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthInterceptor,
+          multi: true,
+        },
+      ],
+    });
+
+    http = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it('should add Authorization header when token is present', () => {
+    mockAuthService.getToken.mockReturnValue('test-token');
+
+    http.get('/test').subscribe();
+
+    const req = httpMock.expectOne('/test');
+    expect(req.request.headers.has('Authorization')).toBe(true);
+    expect(req.request.headers.get('Authorization')).toBe('Bearer test-token');
+    req.flush({});
+  });
+
+  it('should not add Authorization header when token is empty', () => {
+    mockAuthService.getToken.mockReturnValue('');
+
+    http.get('/test').subscribe();
+
+    const req = httpMock.expectOne('/test');
+    expect(req.request.headers.has('Authorization')).toBe(false);
+    req.flush({});
   });
 });
