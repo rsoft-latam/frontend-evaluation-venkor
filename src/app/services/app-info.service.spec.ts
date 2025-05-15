6a030505d639b8fba1abab1715691659b131137b
Ricardo Pari <rikiricardo16@hotmail.com>
2025-05-15 02:39:01 -0400

    feat: unit test for app-info-service

--- src/app/services/app-info.service.spec.ts
+++ src/app/services/app-info.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppInfoService } from './app-info.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
 
 describe('AppInfoService', () => {
   let service: AppInfoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AppInfoService],
+    });
+
     service = TestBed.inject(AppInfoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call GET /api/version and return version data', () => {
+    const mockResponse = { success: true, version: '2.0.1' };
+
+    service.getApiVersion().subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiBaseUrl}/api/version`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
   });
 });
