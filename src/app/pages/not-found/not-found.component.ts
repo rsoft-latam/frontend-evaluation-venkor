58767ab73dba32f0454885be2acf021b7afaa00c
Ricardo Pari <rikiricardo16@hotmail.com>
2025-05-15 10:14:06 -0400

    feat: styles for not-found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -4,12 +4,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-not-found',
   template: `
     <div
-      class="h-[calc(100vh-100px)] mt-[60px] flex flex-col items-center justify-center px-4 text-center"
+      class="h-full flex flex-col items-center justify-center text-center"
     >
       <img
         src="assets/images/404.png"
         alt="404 Not Found"
-        class="w-[50%] sm:w-[50%] md:w-[40%] lg:w-[40%] max-w-[40%]"
       />
     </div>
   `,
