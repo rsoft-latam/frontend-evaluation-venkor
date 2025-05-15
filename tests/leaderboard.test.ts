022c97e5fc2ad5261992cc8b47915a743acc4049
Ricardo Pari <rikiricardo16@hotmail.com>
2025-05-15 10:23:00 -0400

    feat: reorder unit test folder

--- tests/leaderboard.test.ts
+++ tests/leaderboard.test.ts
@@ -1,54 +1,68 @@
 /**
- * 
+ *
  *  THIS IS A TESTING FILE. YOU CAN FREELY MODIFY THE CODE BELOW IN ORDER TO TEST YOUR WORK.
  *  PLEASE DON´T CHANGE THE INTERFACE OF leagueService.js METHODS
  *
  */
 
-import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { LeagueService } from "../src/app/services/league.service";
+import { LeagueService } from '../src/app/services/league.service';
+import { LeaderboardComponent } from 'src/app/pages/leaderboard/leaderboard.component';
+import { NgTableComponent } from 'src/app/shared/ng-table/ng-table.component';
+import { ITeamStats } from 'src/app/shared/models/league.model';
+import { LEADERBOARD_COLUMN_DEFS } from 'src/app/pages/leaderboard/column-defs';
 
-describe("laderboard", () => {
-  let leagueService: LeagueService;
+describe('laderboard', () => {
+  let component: LeaderboardComponent;
+  let fixture: ComponentFixture<LeaderboardComponent>;
+  let mockLeagueService: jest.Mocked<LeagueService>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule, HttpClientTestingModule],
-      providers: [LeagueService]
-    })
-    leagueService = TestBed.get(LeagueService);
+  beforeEach(async () => {
+    mockLeagueService = {
+      fetchData: jest.fn(),
+      getMatches: jest.fn(),
+      getLeaderBoard: jest.fn(),
+    } as unknown as jest.Mocked<LeagueService>;
+
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [NgTableComponent, LeaderboardComponent],
+      providers: [{ provide: LeagueService, useValue: mockLeagueService }],
+    }).compileComponents();
   });
 
-  test('check-leaderboard-teams', async () => {
-    const matches = [
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LeaderboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have columnDefs equal to LEADERBOARD_COLUMN_DEFS', () => {
+    expect(component.columnDefs).toBe(LEADERBOARD_COLUMN_DEFS);
+  });
+
+  it('should call fetchData and getLeaderBoard on init', async () => {
+    const mockLeaderBoard: ITeamStats[] = [
       {
-        matchDate: Date.now(),
-        stadium: 'Maracanã',
-        homeTeam: 'Brazil',
-        awayTeam: 'France',
-        matchPlayed: true,
-        homeTeamScore: 2,
-        awayTeamScore: 1
-      }
+        teamName: 'Team A',
+        matchesPlayed: 5,
+        goalsFor: 12,
+        goalsAgainst: 7,
+        points: 13,
+      },
     ];
-    leagueService.setMatches(matches);
 
-    const leaderboard = leagueService.getLeaderBoard();
+    mockLeagueService.fetchData.mockResolvedValue(undefined);
+    mockLeagueService.getLeaderBoard.mockReturnValue(mockLeaderBoard);
 
-    const firstTeam = leaderboard[0];
-    expect(firstTeam.teamName).toBe('Brazil');
-    expect(firstTeam.matchesPlayed).toBe(1);
-    expect(firstTeam.goalsFor).toBe(2);
-    expect(firstTeam.goalsAgainst).toBe(1);
-    expect(firstTeam.points).toBe(3);
+    await component.ngOnInit();
 
-    const secondTeam = leaderboard[1];
-    expect(secondTeam.teamName).toBe('France');
-    expect(secondTeam.matchesPlayed).toBe(1);
-    expect(secondTeam.goalsFor).toBe(1);
-    expect(secondTeam.goalsAgainst).toBe(2);
-    expect(secondTeam.points).toBe(0);
+    expect(mockLeagueService.getLeaderBoard).toHaveBeenCalled();
+    expect(component.leaderboard).toBe(mockLeaderBoard);
   });
 });
